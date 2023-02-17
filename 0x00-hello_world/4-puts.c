e66c9c8bba493b4a62fb2ab4e9906e8bb7cd134b
Mawuli-Manei <mawuli.manei@gmail.com>
2023-02-17 04:54:04 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,3 +8,8 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+/**
+ * @brief The main function.
+ *
+ * @return int Returns 0 on successful execution.
+ */
