759a9c30ef2a07568de6ae3e5152ec201f34c0c9
Mawuli-Manei <mawuli.manei@gmail.com>
2023-02-17 04:47:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * REturn: 0 (Success)
+ * REturn: 0
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
